fecdace3535e7055d9418a105281095478e400b0
June Hong <junehong69@gmail.com>
2024-11-06 15:59:41 -0800

    Add crosswordPuzzle

---
+++ crosswordPuzzle.cpp
@@ -0,0 +1,94 @@
+
+/*
+PROBLEM:
+
+CodingNinjas has provided a crossword of 10*10 grid. The grid contains '+' or '-' as its cell values. Now, you are also provided with a word list that needs to placed accurately in the grid. Cells marked with '-' are to be filled with word list.
+For example, The following is an example for the input crossword grid and the word list.
++-++++++++
++-++-+++++
++-------++
++-++-+++++
++-++-+++++
++-++-+++++
+++++-+++++
+++++-+++++
+++++++++++
+----------
+CALIFORNIA;NIGERIA;CANADA;TELAVIV
+Output for the given input should be:
++C++++++++
++A++T+++++
++NIGERIA++
++A++L+++++
++D++A+++++
++A++V+++++
+++++I+++++
+++++V+++++
+++++++++++
+CALIFORNIA
+Note: We have provided such test cases that there is only one solution for the given input.
+Input format:
+The first 10 lines of input contain crossword. Each of 10 lines has a character array of size 10. Input characters are either '+' or '-'.
+The next line of input contains the word list, in which each word is separated by ';'. 
+Output format:
+Print the crossword grid, after placing the words of word list in '-' cells.  
+Sample Test Cases:
+Sample Input 1:
++-++++++++
++-++-+++++
++-------++
++-++-+++++
++-++-+++++
++-++-+++++
+++++-+++++
+++++-+++++
+++++++++++
+----------
+CALIFORNIA;NIGERIA;CANADA;TELAVIV
+Sample Output 1:
++C++++++++
++A++T+++++
++NIGERIA++
++A++L+++++
++D++A+++++
++A++V+++++
+++++I+++++
+++++V+++++
+++++++++++
+CALIFORNIA
+*/
+#include <iostream>
+
+using namespace std;
+
+vector<string> crosswordPuzzle(vector<string> crossword, string words) {
+
+}
+
+int main() {
+    vector<string> crossword {
+        "+-++++++++",
+        "+-++++++++",
+        "+-++++++++",
+        "+-----++++",
+        "+-+++-++++",
+        "+-+++-++++",
+        "+++++-++++",
+        "++------++",
+        "+++++-++++",
+        "+++++-++++"
+    };
+    string words = "LONDON;DELHI;ICELAND;ANKARA";
+
+    vector<string> result = crosswordPuzzle(crossword, words);
+    for (size_t i = 0; i < result.size(); i++) {
+        cout << result[i];
+
+        if (i != result.size() - 1) {
+            cout << "\n";
+        }
+    }
+    cout << "\n";
+    
+    return 0;
+}
